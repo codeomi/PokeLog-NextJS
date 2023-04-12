a066e8f6ca427cb03f674d63c6b414c0c538da5b
Omkar Kudale <omkar7890kudale@gmail.com>
2023-04-12 22:31:28 +0530

    Made basic of pokemon description page.

--- components/Card/PokeCard.js
+++ components/Card/PokeCard.js
@@ -1,26 +1,25 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 import styles from "./PokeCard.module.css";
 import Link from "next/link";
 
-function PokeCard({title,serialNo,img,type}) {
+function PokeCard({ title, serialNo, img, type }) {
   return (
-    <><Link className={styles.linktag} href={"/pokemon"}>
-      <div className={styles.container}>
-        <div className={styles.imageContainer}>
-        <Image className={styles.pokeImage}
-      alt="Landscape picture"
-     ></Image>
-        </div>
-        <div className={styles.description}>
-          <div className={styles.serialNo}>001</div>
-          <div className={styles.name}>Rhino</div>
-          <div className={styles.type}>
-            <div className="grass">grass</div>
-            <div className="grass">water</div>
+    <>
+      <Link className={styles.linktag} href={`/pokemon`}>
+        <div className={styles.container}>
+          <div className={styles.imageContainer}>
+            <Image className={styles.pokeImage} alt="Landscape picture"></Image>
+          </div>
+          <div className={styles.description}>
+            <div className={styles.serialNo}>001</div>
+            <div className={styles.name}>Rhino</div>
+            <div className={styles.type}>
+              <div className="grass">grass</div>
+              <div className="grass">water</div>
+            </div>
           </div>
         </div>
-      </div>
       </Link>
     </>
   );
--- pages/pokemon.js
+++ pages/pokemon.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/pages css/PokemonDetails.module.css";
+import Header from "../components/Header/Header";
+import { style } from "@mui/system";
 
-function PokemonDetails() {
+function pokemon() {
   return (
-    <div>PokemonDetails</div>
-  )
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <div className={styles.pokemonName}>Rhino</div>
+          <div className={styles.pokemonSerial}>0002</div>
+        </div>
+        <div className={styles.container1}>
+          <div className={styles.container11}>
+            <div className={styles.imageContainer}>
+              <div>Image goes here</div>
+            </div>
+            <div className="container-1-2">
+              <div className="statistics">Statistics</div>
+            </div>
+          </div>
+          <div className={styles.container2}>
+            <div className={styles.pokemonDescription}>
+              When the bulb on its back grows large, it appears to lose the
+              ability to stand on its hind legs.
+            </div>
+            <div className={styles.container12}>
+              <div className={styles.heightContainer}>
+                Height
+                <div className={styles.heightInput}>5</div>
+              </div>
+              <div className={styles.categoryContainer}>
+                Category
+                <div className={styles.categoryInput}>Grass</div>
+              </div>
+              <div className={styles.weightContainer}>
+                Weight
+                <div className={styles.weightInput}>54kg</div>
+              </div>
+              <div className={styles.abilityContainer}>
+                Ability
+                <div className={styles.abilityInput}>Overgrow</div>
+              </div>
+            </div>
+            <div className={styles.container22}>
+              <div className={styles.pokeType}>Type</div>
+              <div className={styles.types}>
+                <div className="div">Grass</div>
+                <div className="div">Poison</div>
+              </div>
+            </div>
+            <div className={styles.container223}>
+              <div className={styles.pokeWeakness}>Weaknesses</div>
+              <div className={styles.weaknesses}>
+                <div className="div">fire</div>
+                <div className="div">fire</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container-2">Evolutions</div>
+      </div>
+    </>
+  );
 }
 
-export default PokemonDetails
+export default pokemon;
